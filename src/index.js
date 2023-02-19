0750fb8ea3d233866c41fb28a3df982fdde67722
Yevhen <yevhen.yakymenko.fed@gmail.com>
2023-02-19 22:13:53 +0200

    added section Hero + modal + styles

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 (() => {
+  // Mobile menu control
   const headerMenu = document.querySelector('.js-menu-container');
   const menuBtn = document.querySelector('.js-toggle-btn');
   const navLinks = document.querySelectorAll('.nav__link');
@@ -32,6 +33,37 @@
   navLinks.forEach(navLink => navLink.addEventListener('click', onMenuClick));
 
   menuBtn.addEventListener('click', toggleMenu);
+
+  // Modal and form control
+  const modal = document.querySelector('.js-modal');
+  const openFormBtn = document.querySelector('.js-btn-form');
+  const modalForm = document.querySelector('.js-form');
+  const closeModalBtn = document.querySelector('.js-btn-close');
+
+  openFormBtn.addEventListener('click', () => {
+    modal.classList.remove('modal-close');
+    document.body.classList.add('modal-open');
+  });
+
+  modal.addEventListener('click', e => {
+    if (e.target === e.currentTarget || e.target === closeModalBtn) {
+      modal.classList.add('modal-close');
+      document.body.classList.remove('modal-open');
+      modalForm.reset();
+    }
+  });
+
+  modalForm.addEventListener('submit', e => {
+    e.preventDefault();
+
+    new FormData(e.currentTarget).forEach((value, name) =>
+      console.log(`${name}: ${value}`)
+    );
+
+    modal.classList.remove('modal-open');
+
+    e.currentTarget.reset();
+  });
 
   // Close the mobile menu on wider screens if the device orientation changes
   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
